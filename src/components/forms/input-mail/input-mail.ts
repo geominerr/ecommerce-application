8cb5a743db05ae1628e2a408c77b67e47ab8c75c
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-09 15:53:18 +0300

    feat: update input-mail component

--- src/components/forms/input-mail/input-mail.ts
+++ src/components/forms/input-mail/input-mail.ts
@@ -1,12 +1,15 @@
 import BaseComponent from '../../base/base-component/base-component';
 import ErrorHint from '../error-hint/error-hint';
-import { TagNames, Styles, Attributes, Events } from './enum';
+import { TagNames, Styles, Attributes, Contents, Events } from './enum';
 import './input-mail.scss';
 
-type CallbackStub = (inuptValue: string) => string | false;
+// удалить после того как будет util: Validator
+type CallbackStub = (inuptValue: string) => string | null;
 
 class InputMail extends BaseComponent {
   private container: HTMLElement;
+
+  private label: HTMLLabelElement;
 
   private input: HTMLInputElement;
 
@@ -17,6 +20,7 @@ class InputMail extends BaseComponent {
   constructor(validator: CallbackStub) {
     super();
     this.container = this.createElement(TagNames.DIV, Styles.INPUT_CONTAINER);
+    this.label = this.createElement(TagNames.LABEL, Styles.LABEL);
     this.input = this.createElement<HTMLInputElement>(TagNames.INPUT, Styles.INPUT);
     this.errorHint = new ErrorHint();
 
@@ -24,12 +28,16 @@ class InputMail extends BaseComponent {
   }
 
   public createComponent(): this {
-    const { input, errorHint } = this;
+    const { container, label, input, errorHint } = this;
     const errorHintElement: HTMLElement = errorHint.getElement();
 
+    input.setAttribute(Attributes.ID, Attributes.ID_VALUE_INPUT);
     input.setAttribute(Attributes.PLACEHOLDER, Attributes.PLACEHOLDER_VALUE);
+    input.setAttribute(Attributes.NAME, Attributes.NAME_VALUE);
+    label.setAttribute(Attributes.FOR, Attributes.ID_VALUE_INPUT);
+    label.innerText = Contents.LABEL;
 
-    [input, errorHintElement].forEach((el: HTMLElement): void => this.container.append(el));
+    [label, input, errorHintElement].forEach((el: HTMLElement): void => container.append(el));
     this.addInputHadler(input);
 
     return this;
@@ -39,10 +47,14 @@ class InputMail extends BaseComponent {
     return this.container;
   }
 
+  public isValid(): boolean {
+    return !this.input.classList.contains(Styles.INPUT_ERROR);
+  }
+
   private addInputHadler(input: HTMLInputElement): void {
     input.addEventListener(Events.INPUT, (): void => {
       const inputValue: string = input.value;
-      const errorText: string | false = this.validator(inputValue);
+      const errorText: string | null = this.validator(inputValue);
 
       if (errorText) {
         input.classList.add(Styles.INPUT_ERROR);
