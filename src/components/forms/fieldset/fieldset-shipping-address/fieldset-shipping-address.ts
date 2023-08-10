77b1b27a0eb415f9ba55bab9d556ead95f963b98
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-10 22:00:15 +0300

    feat: add field-shipping component

---
+++ src/components/forms/fieldset/fieldset-shipping-address/fieldset-shipping-address.ts
@@ -0,0 +1,63 @@
+import BaseComponent from '../../../base/base-component/base-component';
+import SelectComponent from '../../select/select';
+import InputBase from '../../input-base/input-base';
+import { TagNames, Styles, Contents } from './enum';
+import { CallbackStub } from './fieldset-interfaces';
+import { OPTIONS } from './input-options';
+import './fieldset.scss';
+
+class FieldsetShip extends BaseComponent {
+  private fieldsetElement: HTMLFieldSetElement;
+
+  private legendElement: HTMLLegendElement;
+
+  private select: SelectComponent;
+
+  private inputCity: InputBase;
+
+  private inputStreet: InputBase;
+
+  private inputStreetNumber: InputBase;
+
+  private inputPostal: InputBase;
+
+  constructor(validator: CallbackStub) {
+    super();
+
+    this.fieldsetElement = this.createElement(TagNames.FIELDSET, Styles.FIELDSET);
+    this.legendElement = this.createElement(TagNames.LEGEND, Styles.LEGEND);
+    this.select = new SelectComponent('shipping');
+    this.inputCity = new InputBase(validator, OPTIONS[0]);
+    this.inputStreet = new InputBase(validator, OPTIONS[1]);
+    this.inputStreetNumber = new InputBase(validator, OPTIONS[2]);
+    this.inputPostal = new InputBase(validator, OPTIONS[3]);
+
+    this.createComponent();
+  }
+
+  public getElement(): HTMLElement {
+    return this.fieldsetElement;
+  }
+
+  private createComponent(): void {
+    const {
+      fieldsetElement,
+      legendElement,
+      select,
+      inputCity,
+      inputStreet,
+      inputStreetNumber,
+      inputPostal,
+    } = this;
+
+    legendElement.innerText = Contents.LEGEND;
+    fieldsetElement.append(legendElement);
+
+    [select, inputCity, inputStreet, inputStreetNumber, inputPostal].forEach(
+      (component: InputBase | SelectComponent): void =>
+        fieldsetElement.append(component.getElement())
+    );
+  }
+}
+
+export default FieldsetShip;
