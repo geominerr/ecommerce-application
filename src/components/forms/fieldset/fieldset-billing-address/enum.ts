3e6f169081a966c5585a1b1dac87ea873f636c1c
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-10 21:58:04 +0300

    feat: add fieldset-billing component

---
+++ src/components/forms/fieldset/fieldset-billing-address/enum.ts
@@ -0,0 +1,15 @@
+enum TagNames {
+  FIELDSET = 'fieldset',
+  LEGEND = 'legend',
+}
+
+enum Styles {
+  FIELDSET = 'fieldset',
+  LEGEND = 'legend',
+}
+
+enum Contents {
+  LEGEND = 'Billing address',
+}
+
+export { TagNames, Styles, Contents };
