366d2df511cb58755e930bd177b4ca0f9621c5e1
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-11 09:08:14 +0300

    fix: remove base template before merge

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,5 @@
-import LoginPage from '../pages/login-page/login-page';
-import RegistrationPage from '../pages/registration-page/registration-page';
-
 class App {
-  private loginPage: LoginPage;
-
-  private registartionPage: RegistrationPage;
-
-  constructor() {
-    this.loginPage = new LoginPage();
-    this.registartionPage = new RegistrationPage();
-  }
-
-  public start(): void {
-    // this.loginPage.render();
-    this.registartionPage.render();
-  }
+  public start(): void {}
 }
 
 export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import App from './app/app';
-import './main.scss';
 
 const app: App = new App();
 app.start();
