2f9356d2cfa5502befc6502d3c1217f8f641d33f
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-11 16:55:01 +0300

    feat: add methods to work with pass strength hints

--- src/components/forms/error-hint/enum.ts
+++ src/components/forms/error-hint/enum.ts
@@ -1,10 +1,19 @@
 enum Styles {
   ERROR_HINT = 'error-hint',
   VISIBLE = 'visible',
+  VISIBLE_PASS_STRONG = 'visible--strong-pass',
+  VISIBLE_PASS_MEDIUM = 'visible--medium-pass',
+  VISIBLE_PASS_WEAK = 'visible--weak-pass',
 }
 
 enum TagNames {
   DIV = 'DIV',
 }
 
-export { TagNames, Styles };
+enum StrengthPassword {
+  WEAK = 'Weak password.',
+  MEDIUM = 'Medium password strength.',
+  STRONG = 'Strong password.',
+}
+
+export { TagNames, Styles, StrengthPassword };
--- src/components/forms/error-hint/error-hint.ts
+++ src/components/forms/error-hint/error-hint.ts
@@ -1,5 +1,5 @@
 import BaseComponent from '../../base/base-component/base-component';
-import { TagNames, Styles } from './enum';
+import { TagNames, Styles, StrengthPassword } from './enum';
 import './error-hint.scss';
 
 class ErrorHint extends BaseComponent {
@@ -14,13 +14,40 @@ class ErrorHint extends BaseComponent {
     return this.element;
   }
 
+  public showPassHint(textHint: string): void {
+    let style: string = Styles.VISIBLE_PASS_WEAK;
+
+    switch (textHint) {
+      case StrengthPassword.MEDIUM:
+        style = Styles.VISIBLE_PASS_MEDIUM;
+        break;
+      case StrengthPassword.STRONG:
+        style = Styles.VISIBLE_PASS_STRONG;
+        break;
+    }
+
+    this.removePassHintStyles();
+    this.element.innerText = textHint;
+    this.element.classList.add(style);
+  }
+
   public showErrorText(textError: string): void {
+    this.removePassHintStyles();
+
     this.element.innerText = textError;
     this.element.classList.add(Styles.VISIBLE);
   }
 
   public hideErrorText(): void {
     this.element.classList.remove(Styles.VISIBLE);
+  }
+
+  private removePassHintStyles(): void {
+    this.element.classList.remove(
+      Styles.VISIBLE_PASS_WEAK,
+      Styles.VISIBLE_PASS_MEDIUM,
+      Styles.VISIBLE_PASS_STRONG
+    );
   }
 }
 
