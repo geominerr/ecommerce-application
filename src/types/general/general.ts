637f09166cf48ff0a63ca665c0836b66077eeb48
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-12 11:06:18 +0300

    feat: add ValidationPostal type

--- src/types/general/general.ts
+++ src/types/general/general.ts
@@ -1,3 +1,5 @@
 type ValidationFunction = (value: string) => string | null;
 
-export { ValidationFunction };
+type ValidationPostal = (code: string, country: string) => string | null;
+
+export { ValidationFunction, ValidationPostal };
