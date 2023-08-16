0cf10af700bfeed884434aa04d86cd62822c6e43
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-16 17:25:53 +0300

    refactor: remove extra code

--- src/app/app.ts
+++ src/app/app.ts
@@ -86,7 +86,6 @@ class App {
     );
   }
 
-  // eslint-disable-next-line max-lines-per-function
   public start(): void {
     this.router.start();
 
@@ -106,47 +105,6 @@ class App {
       }
     }
     getData();
-
-    const USER_ACTIONS = new APIUserActions();
-
-    // email: string, firstName: string, lastName: string, dateOfBirth: string YYYY-MM-DD, shippingAddresses: number[], billingAddresses: number[], password: string
-    function register(): void {
-      USER_ACTIONS.registerUser(
-        'Shipiing street',
-        '123-s',
-        'Shipping City',
-        '0000-s',
-        'US',
-        'Billing street',
-        '123-b',
-        'Billing city',
-        '0000-b',
-        'US',
-        'user@mail.com',
-        'FirstuserName',
-        'FirstuserSurname',
-        '2010-11-10',
-        'pAssw0rd!'
-      )
-        .then(() => {
-          console.log('success');
-          // Действия после успешной регистрации
-        })
-        .catch((error) => {
-          console.log('Register error in APP: ', error.message);
-        });
-    }
-    register();
-
-    USER_ACTIONS.loginUser('user@mail.com', 'pAssw0rd!')
-      .then((data) => {
-        console.log('login success', data);
-        console.log(`Hi ${data.firstName} ${data.lastName}`);
-      })
-      .catch((error) => {
-        console.log('Login error in APP: ', error.message);
-      });
-    setTimeout(() => USER_ACTIONS.logoutUser(), 5000);
   }
 }
 
