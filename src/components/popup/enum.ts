ad0c37ed4c43b2bc0f2f72d699243e39d500fe42
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-17 13:58:51 +0300

    feat: add popup component

---
+++ src/components/popup/enum.ts
@@ -0,0 +1,18 @@
+enum TagNames {
+  DIV = 'div',
+  BUTTON = 'button',
+}
+
+enum Styles {
+  POPUP_CONTAINER = 'popup-container',
+  POPUP = 'popup',
+  POPUP_DESCRIPTION = 'popup__description',
+  BTN_CLOSE = 'popup__btn-close',
+  POPUP_VISIBLE = 'popup-container--visible',
+}
+
+enum Events {
+  CLICK = 'click',
+}
+
+export { TagNames, Styles, Events };
