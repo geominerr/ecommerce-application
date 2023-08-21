3185393db06a3ee08cd908abada8ed98890da7cd
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-21 14:25:45 +0300

    feat: add footer component

---
+++ src/components/footer/enum.ts
@@ -0,0 +1,22 @@
+enum TagNames {
+  DIV = 'div',
+  FOOTER = 'footer',
+  A = 'a',
+}
+
+enum Styles {
+  FOOTER = 'footer-new',
+  CONTAINER = 'container-footer',
+  LINK = 'link-footer',
+}
+
+enum Content {
+  LINK_ABOUT_US = 'About us',
+}
+
+enum Attributes {
+  HREF = 'href',
+  HREF_VALUE_ABOUT_US = '/about_us',
+}
+
+export { TagNames, Styles, Content, Attributes };
