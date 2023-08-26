e1b11830c6138a921e44fb82937252bf560dad30
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-26 22:39:29 +0300

    feat: add getProductById method

--- src/api/api-interfaces.ts
+++ src/api/api-interfaces.ts
@@ -78,3 +78,48 @@ export interface IUserData {
   defaultShippingAddress?: number;
   defaultBillingAddress?: number;
 }
+
+export interface IProductResponse {
+  id: string;
+  name: { en: string };
+  description: { en: string };
+  masterVariant: IProductVariant;
+}
+
+interface IProductVariant {
+  prices: IProductPrice[];
+  images: IProductImage[];
+  attributes: IProductAttribute[];
+}
+
+interface IProductPrice {
+  value: {
+    currencyCode: string;
+    centAmount: number;
+    fractionDigits: number;
+  };
+  discounted?: {
+    value: {
+      currencyCode: string;
+      centAmount: number;
+      fractionDigits: number;
+    };
+    discount: {
+      id: string;
+    };
+  };
+}
+
+interface IProductImage {
+  url: string;
+  label: string;
+  dimensions: {
+    w: number;
+    h: number;
+  };
+}
+
+interface IProductAttribute {
+  name: string;
+  value: string;
+}
