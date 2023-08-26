fb9c551bd651f5aa0a0a82f8d37813a07d423bf8
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-26 22:51:02 +0300

    feat: integrate temporarily detail-page

--- src/pages/amplifiers/amplifiers.ts
+++ src/pages/amplifiers/amplifiers.ts
@@ -1,19 +1,23 @@
 import TemplateView from '../template-view/template-view';
 import './amplifiers.scss';
 import { APIProductActions } from '../../api/product-actions/api-product-actions';
+import DetailPage from '../detail-page/detail-page';
 
 export default class Amplifiers extends TemplateView {
   private api: APIProductActions;
 
+  private detailPage: DetailPage;
+
   constructor(api: APIProductActions) {
     super();
     this.api = api;
+    this.detailPage = new DetailPage();
   }
 
   private documentTitle: string = 'Amplifiers';
 
-  public async getHtml(): Promise<string> {
-    return `<div class="amplifiers">This is Amplifiers page</div>`;
+  public async getHtml(): Promise<HTMLElement> {
+    return this.detailPage.getElement();
   }
 
   public setTitle(): void {
