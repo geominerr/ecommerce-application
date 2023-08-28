fdcffbc4bafbc7c4d381d6d18ca19e79920e07a7
Maksym Voloshyn <mv.webdew@gmail.com>
2023-08-28 22:18:59 +0300

    feat: add navbar-breadcrumb component

---
+++ src/components/navbar-breadcrumb/enum.ts
@@ -0,0 +1,18 @@
+enum TagNames {
+  DIV = 'div',
+  NAV = 'nav',
+  A = 'a',
+  IMG = 'img',
+  SPAN = 'span',
+}
+
+enum Styles {
+  NAVBAR = 'navbar-container',
+  LINK_WRAPPER = 'link-wrapper',
+  LINK = 'navbar-link',
+  LINKS_CONTAINER = 'links-container',
+  IMAGE_WRAPPER = 'home-logo-wrapper',
+  IMAGE = 'logo-image',
+}
+
+export { TagNames, Styles };
