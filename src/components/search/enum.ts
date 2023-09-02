9e423f554d2bdf69b09431f18efbb6911583f9b3
Maksym Voloshyn <mv.webdew@gmail.com>
2023-09-02 13:01:13 +0300

    feat: add search component

---
+++ src/components/search/enum.ts
@@ -0,0 +1,12 @@
+enum TagNames {
+  DIV = 'div',
+  INPUT = 'input',
+}
+
+enum Styles {
+  SEARCH_CONTAINER = 'search-container',
+  INPUT = 'input-search',
+  HINT = 'search-hint',
+}
+
+export { TagNames, Styles };
