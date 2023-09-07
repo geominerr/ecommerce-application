a0c9a8b058c0393497ce12c0da050275f3ff7774
Maksym Voloshyn <mv.webdew@gmail.com>
2023-09-07 16:27:54 +0300

    feat: add buttons

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,16 +1,42 @@
 import TemplateView from '../template-view/template-view';
-import './cart.scss';
-
 import { APIAcceesToken } from '../../api/api-access-token'; // Удалить
 import { IAnonymousResponse } from '../../api/api-interfaces';
+import ButtonPage from '../catalog/buttons/button/button';
+import APICartActions from '../../api/cart-actions/api-cart-actions';
+import './cart.scss';
 
 export default class Cart extends TemplateView {
   private api: APIAcceesToken; // Удалить
 
+  private apiCart: APICartActions;
+
+  private container: HTMLElement;
+
+  private buttonCreateCart: ButtonPage;
+
+  private buttonAddProduct: ButtonPage;
+
   constructor() {
     super();
+    this.container = this.createElement('div', 'cart');
+    this.buttonCreateCart = new ButtonPage('catalog');
+    this.buttonAddProduct = new ButtonPage('catalog');
+    this.createComponent();
     this.api = new APIAcceesToken(); // Удалить
-    this.getAnon(); // Удалить
+    this.apiCart = new APICartActions();
+  }
+
+  private createComponent(): void {
+    const { container, buttonAddProduct, buttonCreateCart } = this;
+    const buttonCreate = buttonCreateCart.getElement();
+    const buttonAdd = buttonAddProduct.getElement();
+
+    buttonCreate.innerText = 'Create cart';
+    buttonAdd.innerText = 'Add product';
+
+    [buttonCreate, buttonAdd].forEach((el) => container.append(el));
+
+    this.addClickHandler(buttonCreateCart.getElement());
   }
 
   // Метод ниже исключительно для теста api. Его необходимо удалить по окончанию настройки.
@@ -24,11 +50,25 @@ export default class Cart extends TemplateView {
 
   private documentTitle: string = 'Cart';
 
-  public async getHtml(): Promise<string> {
-    return `<div class="cart">This is Cart page</div>`;
+  public async getHtml(): Promise<HTMLElement> {
+    this.getAnon();
+    return this.container;
   }
 
   public setTitle(): void {
     document.title = this.documentTitle;
+  }
+
+  private createElement<T extends HTMLElement>(tagName: string, style: string): T {
+    const element: T = document.createElement(tagName) as T;
+    element.classList.add(style);
+
+    return element;
+  }
+
+  private addClickHandler(createCart: HTMLElement): void {
+    createCart.addEventListener('click', () => {
+      this.apiCart.createCart();
+    });
   }
 }
