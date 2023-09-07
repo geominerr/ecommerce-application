4491dcc38c829cda4f0f9e9d94c436c651223744
Boris <boris@MacBook-Air-Boris.local>
2023-09-07 17:19:39 +0400

    feat: add anon token to localstorage if not exisits

--- src/api/cart-actions/cart-actions.ts
+++ src/api/cart-actions/cart-actions.ts
@@ -2,5 +2,27 @@ import { APIAcceesToken } from '../api-access-token';
 import { IAnonymousResponse } from '../api-interfaces';
 
 export class APICartActions {
-  // add methods for anon cart and user's cart
+  private APIAcceesToken: APIAcceesToken;
+
+  constructor() {
+    this.APIAcceesToken = new APIAcceesToken();
+  }
+
+  // Метод ниже исключительно для теста api. Его необходимо удалить по окончанию настройки.
+  public async getAnon(): Promise<void> {
+    const storageAnonymousToken = localStorage.getItem('anonymousAccessToken');
+
+    if (!storageAnonymousToken) {
+      const AnonymousToken: IAnonymousResponse = await this.APIAcceesToken.getAnonymousToken();
+      localStorage.setItem('anonymousAccessToken', AnonymousToken.access_token);
+      localStorage.setItem('anonymousRefreshToken', String(AnonymousToken.refresh_token));
+    }
+
+    console.log('anon: ', storageAnonymousToken);
+
+    // TODO: добавить рефрешь если токен просрочился. Нужно проверить, если он просрочился.
+
+    // const refresh = await this.APIAcceesToken.refreshToken(anon.refresh_token);
+    // console.log('refreshed: ', refresh);
+  }
 }
