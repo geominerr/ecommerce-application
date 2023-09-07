8ec80b2e83ffa521e782f229252298f0ed51f924
Boris <boris@MacBook-Air-Boris.local>
2023-09-07 15:01:59 +0400

    feat: add empty class for cart actions

---
+++ src/api/cart-actions/cart-actions.ts
@@ -0,0 +1,6 @@
+import { APIAcceesToken } from '../api-access-token';
+import { IAnonymousResponse } from '../api-interfaces';
+
+export class APICartActions {
+  // add methods for anon cart and user's cart
+}
