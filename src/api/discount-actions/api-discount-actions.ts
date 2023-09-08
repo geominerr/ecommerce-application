0fcd27be4cae8d74ce43ab1f0869b24c4ff98481
Maksym Voloshyn <mv.webdew@gmail.com>
2023-09-08 14:58:04 +0300

    feat: add interfaces

--- src/api/discount-actions/api-discount-actions.ts
+++ src/api/discount-actions/api-discount-actions.ts
@@ -1,5 +1,6 @@
 import { APIAcceesToken } from '../api-access-token';
 import { CTP_API_URL, CTP_PROJECT_KEY } from '../api-env-constants';
+import { IResponseDiscount } from './api-discount-actions-interfaces';
 
 const API_ACCESS_TOKEN = new APIAcceesToken();
 
@@ -14,14 +15,22 @@ class APIDiscountActions {
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       };
 
-      const response = await fetch(`${CTP_API_URL}/${CTP_PROJECT_KEY}/discount-codes`, {
+      const response: Response = await fetch(`${CTP_API_URL}/${CTP_PROJECT_KEY}/discount-codes`, {
         method: 'GET',
         headers: headers,
       });
 
-      const projectData = await response.json();
+      const discounts: IResponseDiscount = await response.json();
       // покажет discountCodeS которые можно применит
-      console.log(projectData);
+      for (const discount of discounts.results) {
+        const name = discount.name.en;
+        const code = discount.code;
+        const id = discount.id;
+
+        console.log({ name, code, id });
+      }
+
+      console.log(discounts);
     } catch (error) {
       throw error;
     }
