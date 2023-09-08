2814488c1db58073edff3a741448131e8d1b3bc2
Boris <boris@MacBook-Air-Boris.local>
2023-09-08 18:47:12 +0400

    feat: add enum for about us page

---
+++ src/pages/abouts-us/enum.ts
@@ -0,0 +1,19 @@
+enum TagNames {
+  DIV = 'div',
+  NAV = 'nav',
+  A = 'a',
+  IMG = 'img',
+  SPAN = 'span',
+}
+
+enum Styles {
+  CONTAINER = 'about-us-container',
+  DEVCARD = 'devcard',
+}
+
+enum Attributes {
+  SRC = 'src',
+  HREF = 'href',
+}
+
+export { TagNames, Styles, Attributes };
