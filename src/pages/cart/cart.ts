69250bb6429e62bae4b009df0e53766c747610a0
Maksym Voloshyn <mv.webdew@gmail.com>
2023-09-09 23:18:09 +0300

    feat: add basket-page[uncompleted]

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,74 +1,137 @@
 import TemplateView from '../template-view/template-view';
-import ButtonPage from '../catalog/buttons/button/button';
-import APICartActions from '../../api/cart-actions/api-cart-actions';
-import APIDiscountActions from '../../api/discount-actions/api-discount-actions';
+import APICart from '../../api/cart-actions/api-cart-actions';
+import LineItem from './line-item/line-item';
+import EMPTY_CART from './empty-cart';
+import { converteResponseCartData } from '../../utils/response-converter/response-converter';
+import { TagNames, Styles, Content } from './enum';
 import './cart.scss';
 
-const api = new APIDiscountActions();
-
 export default class Cart extends TemplateView {
-  private apiCart: APICartActions;
+  private apiCart = APICart;
+
+  private documentTitle: string = 'Cart';
 
   private container: HTMLElement;
 
-  private buttonCreateCart: ButtonPage;
+  private title: HTMLHeadElement;
 
-  private buttonAddProduct: ButtonPage;
+  private lineItemContainer: HTMLElement;
 
-  private buttonGetCart: ButtonPage;
+  private buttonClearCart: HTMLElement;
 
-  private buttonRemoveProd: ButtonPage;
+  private buttonCatalog: HTMLAnchorElement;
 
-  private buttonGetDiscount: ButtonPage;
+  private buttonBuy: HTMLElement;
+
+  private totalPrice: HTMLElement;
+
+  private totalPriceDiscount: HTMLElement;
+
+  private promoGroup: HTMLElement;
+
+  private buttonGroup: HTMLElement;
 
   constructor() {
     super();
-    this.container = this.createElement('div', 'cart');
-    this.buttonCreateCart = new ButtonPage('catalog');
-    this.buttonAddProduct = new ButtonPage('catalog');
-    this.buttonGetCart = new ButtonPage('catalog');
-    this.buttonRemoveProd = new ButtonPage('catalog');
-    this.buttonGetDiscount = new ButtonPage('catalog');
+    this.container = this.createElement(TagNames.DIV, Styles.CONTAINER);
+    this.title = this.createElement(TagNames.H2, Styles.TITLE);
+    this.lineItemContainer = this.createElement(TagNames.DIV, Styles.LINE_ITEM_CONTAINER);
+    this.buttonClearCart = this.createElement(TagNames.BUTTON, Styles.BTN);
+    this.buttonCatalog = this.createElement(TagNames.A, Styles.BTN);
+    this.buttonBuy = this.createElement(TagNames.BUTTON, Styles.BTN);
+    this.totalPrice = this.createElement(TagNames.P, Styles.PRICE);
+    this.totalPriceDiscount = this.createElement(TagNames.P, Styles.DISCOUNT_PRICE);
+    this.promoGroup = this.createPromoGroup();
+    this.buttonGroup = this.createButtonGroup();
     this.createComponent();
-    this.apiCart = new APICartActions();
   }
 
   private createComponent(): void {
-    const {
-      container,
-      buttonAddProduct,
-      buttonCreateCart,
-      buttonGetCart,
-      buttonRemoveProd,
-      buttonGetDiscount,
-    } = this;
-    const buttonCreate = buttonCreateCart.getElement();
-    const buttonAdd = buttonAddProduct.getElement();
-    const buttonGet = buttonGetCart.getElement();
-    const buttonRemove = buttonRemoveProd.getElement();
-    const buttonGetD = buttonGetDiscount.getElement();
+    const { container, title, lineItemContainer, promoGroup, buttonGroup } = this;
 
-    buttonCreate.innerText = 'Create cart';
-    buttonAdd.innerText = 'Add product';
-    buttonGet.innerText = 'Get cart';
-    buttonRemove.innerText = 'Remove';
-    buttonGetD.innerText = 'get Discount';
-
-    [buttonCreate, buttonGet, buttonAdd, buttonRemove, buttonGetD].forEach((el) =>
-      container.append(el)
-    );
+    title.innerText = Content.TITLE;
+    [title, lineItemContainer, promoGroup, buttonGroup].forEach((el) => container.append(el));
 
     this.addClickHandler();
   }
 
-  private documentTitle: string = 'Cart';
-
   public async getHtml(): Promise<HTMLElement> {
+    // под капотом обновление все полей, которые могут изменятся, продукты, общая цена и тд и тп
+    await this.updateCart()
+      .then(() => console.log('Ok'))
+      .catch((err) => console.log(err));
+
     return this.container;
   }
 
   public setTitle(): void {
     document.title = this.documentTitle;
+  }
+
+  private async updateCart(): Promise<void> {
+    const { lineItemContainer, totalPrice, promoGroup, buttonGroup } = this;
+    lineItemContainer.innerHTML = '';
+    promoGroup.style.display = 'none';
+    buttonGroup.style.display = 'none';
+
+    try {
+      const carData = await this.apiCart.getCart();
+
+      if (carData) {
+        const convertedData = converteResponseCartData(carData);
+        totalPrice.innerText = convertedData.totalPrice;
+
+        if (!convertedData.products.length) {
+          lineItemContainer.innerHTML = EMPTY_CART;
+
+          return;
+        }
+
+        promoGroup.style.display = 'flex';
+        buttonGroup.style.display = 'flex';
+
+        convertedData.products.forEach((product) => {
+          const lineItem = new LineItem(product).getElement();
+          lineItemContainer.append(lineItem);
+        });
+      }
+    } catch {
+      throw new Error('Cart fetch problem');
+    }
+  }
+
+  private createButtonGroup(): HTMLElement {
+    const { buttonCatalog, buttonClearCart } = this;
+    const container = this.createElement(TagNames.DIV, Styles.BTN_GROUP);
+    const orderGroup = this.createOrderGroup();
+
+    buttonClearCart.classList.add(Styles.BTN_CLEAR);
+    buttonCatalog.innerText = Content.BTN_CONTINUE;
+    buttonClearCart.innerText = Content.BTN_CLEAR;
+    buttonCatalog.href = '/catalog';
+    [buttonCatalog, buttonClearCart, orderGroup].forEach((el) => container.append(el));
+
+    return container;
+  }
+
+  private createOrderGroup(): HTMLElement {
+    const { buttonBuy, totalPrice, totalPriceDiscount } = this;
+    const container = this.createElement(TagNames.DIV, Styles.ORDER_GROUP);
+    const priceWrapper = this.createElement(TagNames.DIV, Styles.PRICE_WRAPPER);
+
+    buttonBuy.innerText = Content.BTN_ORDER;
+    buttonBuy.classList.add(Styles.BTN_ORDER);
+
+    [totalPrice, totalPriceDiscount].forEach((el) => priceWrapper.append(el));
+    [priceWrapper, buttonBuy].forEach((el) => container.append(el));
+
+    return container;
+  }
+
+  private createPromoGroup(): HTMLElement {
+    const container = this.createElement(TagNames.DIV, Styles.PROMO_GROUP);
+    container.innerText = 'PromoCode';
+    return container;
   }
 
   private createElement<T extends HTMLElement>(tagName: string, style: string): T {
@@ -83,8 +146,11 @@ export default class Cart extends TemplateView {
       const { target } = e;
 
       if (target instanceof HTMLElement) {
-        if (target.innerText === 'Create cart') {
-          this.apiCart.createCart();
+        if (target.classList.contains(Styles.BTN_CLEAR)) {
+          this.apiCart
+            .removeCart()
+            .then(() => this.updateCart().catch((err) => console.log(err)))
+            .catch((err) => console.log(err));
         }
 
         if (target.innerText === 'Add product') {
@@ -105,7 +171,6 @@ export default class Cart extends TemplateView {
         }
 
         if (target.innerText === 'get Discount') {
-          api.getDiscountCodes();
           this.apiCart.addDicsount('max');
         }
       }
---
+++ src/pages/cart/empty-cart.ts
@@ -0,0 +1,8 @@
+const EMPTY_CART = `<div class="empty-cart">
+<p class="empty-cart__message">
+Sorry, your cart is empty, please follow the link and find yourself in the music universe!
+</p>
+<a href="/catalog" class="empty-cart__link">Go to catalog</a>
+</div>`;
+
+export default EMPTY_CART;
